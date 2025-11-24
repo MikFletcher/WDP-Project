e6280a907b578fd434201757e74e7db7991e849a
mfletcher <mikaelarfletcher0130@gmail.com>
2025-11-24 16:19:21 -0500

    Worked on post js

---
+++ Server/models/post.js
@@ -0,0 +1,14 @@
+const posts = [
+    {
+        postId: 54321,
+        postText: "This is a post hi welcome to my post",
+        postTitle: "TITLE POST!",
+        postImage: "idk!"
+    }
+]
+
+function getAllPosts(){
+    return posts
+}
+
+module.exports = {getAllPosts}
