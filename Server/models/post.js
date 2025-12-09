946919b96caab054249a53c76dc4981a2a21205c
mfletcher <mikaelarfletcher0130@gmail.com>
2025-12-09 18:19:18 -0500

    Added MySQL, created entity tables in models folder

--- Server/models/post.js
+++ Server/models/post.js
@@ -1,11 +1,18 @@
-const posts = [
-    {
-        postId: 54321,
-        postText: "This is a post hi welcome to my post",
-        postTitle: "TITLE POST!",
-        postImage: "idk!"
-    }
-]
+const con = require("./db_connect")
+
+async function createPostTable(){
+    let sql = `
+        CREATE TABLE IF NOT EXISTS Post(
+            PostID INT NOT NULL AUTO_INCREMENT,
+            PostTitle VARCHAR(255) NOT NULL,
+            PostText VARCHAR(max) NOT NULL,
+            UserID INT,
+            CONSTRAINT postPK PRIMARY KEY(PostID),
+            CONSTRAINT userFK FOREIGN KEY(userID) REFERENCES User(UserID)
+        );
+    `
+    await con.query(sql)
+}
 
 function getAllPosts(){
     return posts
--- Server/models/user.js
+++ Server/models/user.js
@@ -1,16 +1,28 @@
-const users = [
-    {
-        userId: 12345,
-        userName: "personGuy734",
-        password: "pswdhere",
-        email: "email@place.com",
-        fullName: "John Doe",
-        birthdate: "02/16/95"
-    }
-]
+const con = require("./db_connect")
 
-function getAllUsers(){
-    return users
+async function createUsertable(){
+    let sql = `
+        CREATE TABLE IF NOT EXISTS User(
+            UserID INT NOT NULL AUTO_INCREMENT,
+            Username VARCHAR(25) NOT NULL UNIQUE,
+            Password VARCHAR(25) NOT NULL,
+            Email VARCHAR(255) NOT NULL UNIQUE,
+            FirstName VARCHAR(255) NOT NULL
+            LastName VARCHAR(255) NOT NULL
+            CONSTRAINT userPK PRIMARY KEY(userID)
+        );
+    `
+    await con.query(sql)
+}
+
+createUsertable()
+
+async function getAllUsers(){
+    let sql = `
+    SELECT * FROM User;
+    `
+
+    return await con.query(sql)
 }
 
 module.exports = {getAllUsers}
